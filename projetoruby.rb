e73373b7557a6d9a280a1d60f6ec4275bc32be4e
Silas Vieira <silas_vieira@outlook.com>
2023-03-31 19:57:14 -0300

    Primeiro Desafio Ruby Developer
    Primeiro Desafio Ruby Developer

---
+++ projetoruby.rb
@@ -0,0 +1,14 @@
+#Primeiro projeto ruby na _.Dio
+
+puts "Digite seu nome: "
+puts "===" * 15
+
+nome = gets.chomp
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp
+puts "Sua idade: "
+idade = gets.chomp.to_i
+puts "===" * 15
+puts "Olá seu nome e sobrenome é: #{nome} #{sobrenome}, e sua idade #{idade}."
+puts "===" * 15
+
